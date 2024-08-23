c745016dbcd1746c889cc71cb5d418af304f917c
“ellearz” <earzybaeva@gmail.com>
2024-08-23 12:04:57 +0200

    Initial Commit

---
+++ VacationinVegas/VacationinVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationinVegasApp.swift
+//  VacationinVegas
+//
+//  Created by Elnura Arzybaeva on 23.08.24.
+//
+
+import SwiftUI
+
+@main
+struct VacationinVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VacationinVegasUITests/VacationinVegasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VacationinVegasUITestsLaunchTests.swift
+//  VacationinVegasUITests
+//
+//  Created by Elnura Arzybaeva on 23.08.24.
+//
+
+import XCTest
+
+final class VacationinVegasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
